b5c7ff4a4503f1d7e3ac1a4c19ea4beb0d95e80d
VitaliiFataiev <vitaliy2004and@gmail.com>
2024-05-20 14:01:27 +0300

    Update tickets.1.py

--- tickets.1.py
+++ tickets.1.py
@@ -104,37 +104,3 @@ elif choice == "Вийти":
     save_to_file()  # Збереження перед виходом
     st.write("Дякуємо за використання нашої платформи!")
 
-[
-    {
-        "event_name": "Концерт Imagine Dragons",
-        "event_date": "10.м.2024",
-        "price": 1200,
-        "location": "НСК \"Олімпійський\", Київ"
-    },
-    {
-        "event_name": "Фестиваль Atlas Weekend",
-        "event_date": "27.м.2024",
-        "price": 2500,
-        "location": "ВДНГ, Київ"
-    },
-    {
-        "event_name": "Опера \"Кармен\"",
-        "event_date": "15.м.2024",
-        "price": 800,
-        "location": "Одеський національний академічний театр опери та балету"
-    },
-    {
-        "event_name": "Виступ Cirque du Soleil",
-        "event_date": "05.м.2024",
-        "price": 1500,
-        "location": "Палац спорту, Київ"
-    },
-    {
-        "event_name": "Концерт Монатика",
-        "event_date": "25.м.2024",
-        "price": 700,
-        "location": "Арена Львів, Львів"
-    }
-]
-
-перевір
